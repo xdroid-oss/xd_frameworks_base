0bc3f5e00c7d1f93a36c923b06b7c81b5180d9a7
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2022-01-10 09:47:15 +0530

    base: Allow disabling ripple effect on unlock [1/2]
    neobuddy89:
    * Reverse logic. Enabled animation by default.
    
    ArrowOS:
    * Add keys into backup and validators.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Idecaa59547159d0a2b0ecdb55a207377bd466710

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5545,6 +5545,12 @@ public final class Settings {
         public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
 
         /**
+         * Whether to enable the ripple animation on fingerprint unlock
+         * @hide
+         */
+        public static final String ENABLE_RIPPLE_EFFECT = "enable_ripple_effect";
+
+        /**
          * These are all public system settings
          *
          * @hide
--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -89,5 +89,6 @@ public class SystemSettings {
         Settings.System.DOZE_ON_CHARGE,
         Settings.System.KEYGAURD_MEDIA_ART,
         Settings.System.LOCKSCREEN_BATTERY_INFO,
+        Settings.System.ENABLE_RIPPLE_EFFECT,
     };
 }
