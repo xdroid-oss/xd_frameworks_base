03a5e35c02e797c1523e8b0dde9a852ad0946bc8
spezi77 <spezi7713@gmx.net>
2022-03-12 12:29:14 +0100

    QuickStatusBarHeader: Fix forbidden static imports.
    QuickStatusBarHeader.java:19: error: static
    import only from classes and interfaces
    import static com.android.systemui.BatteryMeterView.BATTERY_STYLE_CIRCLE;
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -16,9 +16,6 @@ package com.android.systemui.qs;
 
 import static android.app.StatusBarManager.DISABLE2_QUICK_SETTINGS;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
-import static com.android.systemui.BatteryMeterView.BATTERY_STYLE_CIRCLE;
-import static com.android.systemui.BatteryMeterView.BATTERY_STYLE_DOTTED_CIRCLE;
-import static com.android.systemui.BatteryMeterView.BATTERY_STYLE_FULL_CIRCLE;
 
 import android.content.Context;
 import android.content.res.Configuration;
