SkylineUI-Reborn/frameworks_base

base: Allow setting wake for notifications separately from doze [1/2]
The strings were misleading. Disabling wake for notifications actually entirely disables doze.
Add a new setting to configure wake for notification separately and amend doze switch strings accordingly

Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -246,7 +246,7 @@ public class DozeTriggers implements DozeMachine.Part {
             return;
         }
         mNotificationPulseTime = SystemClock.elapsedRealtime();
-        if (!mConfig.pulseOnNotificationEnabled(mUserTracker.getUserId())) {
+        if (!mConfig.userPulseOnNotificationEnabled(mUserTracker.getUserId())) {
             runIfNotNull(onPulseSuppressedListener);
             mDozeLog.tracePulseDropped("pulseOnNotificationsDisabled");
             return;

xdroid-oss/xd_frameworks_base

Change-Id: I3b443953a2aef43c49c39144ea5ec52475e7e59d

packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -251,7 +251,7 @@ public class DozeTriggers implements DozeMachine.Part {
             return;
         }
         mNotificationPulseTime = SystemClock.elapsedRealtime();
-        if (!mConfig.pulseOnNotificationEnabled(mSelectedUserInteractor.getSelectedUserId())) {
+        if (!mConfig.userPulseOnNotificationEnabled(mSelectedUserInteractor.getSelectedUserId())) {
             runIfNotNull(onPulseSuppressedListener);
             mDozeLog.tracePulseDropped("pulseOnNotificationsDisabled");
             return;